eeedbbf58d37c076773f1e88010b9946198cf792
Dave1901-sudo <U22244804@utp.edu.pe>
2025-04-28 18:05:56 -0500

    Subiendo cambios al index, subiendo animaciones, contenido al index

--- js/archivos_carga.js
+++ js/archivos_carga.js
@@ -16,3 +16,31 @@ fetch('footer.html')
     document.getElementById('footer').innerHTML = data;
 });
 //continuara...
+//carrousel
+const swiper = new Swiper(".mySwiper", {
+slidesPerView: 1,
+spaceBetween: 30,
+loop: true,
+grabCursor: true,
+centeredSlides: true,
+autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+},
+pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+},
+navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+},
+breakpoints: {
+    768: {
+    slidesPerView: 2,
+    },
+    992: {
+    slidesPerView: 3,
+    },
+}
+});
